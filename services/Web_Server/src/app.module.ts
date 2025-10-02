f75d60d52a1963ff553e95e30a51c53aa50282ed
LeeinUITk17 <22521276@gm.uit.edu.vn>
2025-10-02 10:09:29 +0700

    feat: set up connection a both server , module district

--- services/Web_Server/src/app.module.ts
+++ services/Web_Server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { DistrictsModule } from './districts/districts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CloudinaryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    HttpModule.register({
+      timeout: 8000,
+      maxRedirects: 5,
+    }),
+    PrismaModule,
+    AuthModule,
+    CloudinaryModule,
+    DistrictsModule,
+  ],
 })
 export class AppModule {}
