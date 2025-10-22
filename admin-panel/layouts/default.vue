3cc3e366f676b49fe0728865401252a70f228899
LeeinUITk17 <22521276@gm.uit.edu.vn>
2025-10-22 16:31:26 +0700

    fix: refix layout , last commit admin panel

--- admin-panel/layouts/default.vue
+++ admin-panel/layouts/default.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="flex h-screen bg-gray-900 text-gray-100 font-sans">
+    <div
+        class="flex h-screen text-gray-100 font-sans"
+        :style="{
+            backgroundImage: 'url(https://res.cloudinary.com/dbonwxmgl/image/upload/v1761124311/gg_jefzr4.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed'
+        }"
+    >
         <AppSidebar />
+
         <div class="flex flex-1 flex-col overflow-hidden">
             <AppHeader />
-            <main class="flex-1 overflow-y-auto bg-gray-800 p-6 md:p-8">
+
+            <main class="flex-1 overflow-y-auto bg-gray-800/5 backdrop-blur-sm p-6 md:p-8">
                 <slot />
             </main>
         </div>
