983d711da743984f19ad03473f16dde3be78a26f
vanthaita <thaitv225@gmail.com>
2025-12-13 20:09:43 +0700

    refactor(gis-server): gateway

--- services/GIS_Server/src/app.module.ts
+++ services/GIS_Server/src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { CommonModule } from './common/common.module';
+import { PrismaModule } from './infra/prisma/prisma.module';
+import { CommonModule } from './shared/common.module';
 import { ConfigModule } from '@nestjs/config';
-import { DistrictsModule } from './districts/districts.module';
-import { WardsModule } from './wards/wards.module';
-import { InfrastructuresModule } from './infrastructures/infrastructures.module';
-import { PopulationsModule } from './populations/populations.module';
-import { TerrainsModule } from './terrains/terrains.module';
-import { AirQualitiesModule } from './air-qualities/air-qualities.module';
-import { WaterQualitiesModule } from './water-qualities/water-qualities.module';
-import { TrafficsModule } from './traffics/traffics.module';
-import { AccidentsModule } from './accidents/accidents.module';
-import { PublicTransportsModule } from './public-transports/public-transports.module';
-import { LandUsesModule } from './land-uses/land-uses.module';
-import { UrbanPlansModule } from './urban-plans/urban-plans.module';
-import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { AnalyticsModule } from './analytics/analytics.module';
-import { GlobalRabbitMQModule } from './common/rabbitmq/rabbitmq.module';
+import { DistrictsModule } from './modules/districts/districts.module';
+import { WardsModule } from './modules/wards/wards.module';
+import { InfrastructuresModule } from './modules/infrastructures/infrastructures.module';
+import { PopulationsModule } from './modules/populations/populations.module';
+import { TerrainsModule } from './modules/terrains/terrains.module';
+import { AirQualitiesModule } from './modules/air-qualities/air-qualities.module';
+import { WaterQualitiesModule } from './modules/water-qualities/water-qualities.module';
+import { TrafficsModule } from './modules/traffics/traffics.module';
+import { AccidentsModule } from './modules/accidents/accidents.module';
+import { PublicTransportsModule } from './modules/public-transports/public-transports.module';
+import { LandUsesModule } from './modules/land-uses/land-uses.module';
+import { UrbanPlansModule } from './modules/urban-plans/urban-plans.module';
+import { CloudinaryModule } from './infra/cloudinary/cloudinary.module';
+import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { GlobalRabbitMQModule } from './shared/rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
