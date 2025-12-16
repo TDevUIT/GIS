54c5d4939a0b651dadc23b247644e04f67be7192
vanthaita <thaitv225@gmail.com>
2025-12-16 13:46:03 +0700

    refactor(web-server): remove analytics and water-qualities modules

--- services/Web_Server/src/modules/water-qualities/dto/update-water-quality.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWaterQualityDto } from './create-water-quality.dto';
-
-export class UpdateWaterQualityDto extends PartialType(CreateWaterQualityDto) {}
