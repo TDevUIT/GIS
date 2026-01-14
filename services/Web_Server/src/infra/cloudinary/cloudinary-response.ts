a04b598728dc6a30926e6eb0061600f2503c7903
vanthaita <thaitv225@gmail.com>
2026-01-14 12:54:04 +0700

    refactor(web): Remove unnecessary eslint-disable comments

--- services/Web_Server/src/infra/cloudinary/cloudinary-response.ts
+++ services/Web_Server/src/infra/cloudinary/cloudinary-response.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 import { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
 
 export type CloudinaryResponse = UploadApiResponse | UploadApiErrorResponse;
--- services/Web_Server/src/infra/cloudinary/cloudinary.service.ts
+++ services/Web_Server/src/infra/cloudinary/cloudinary.service.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/prefer-promise-reject-errors */
 import { Injectable, Logger } from '@nestjs/common';
